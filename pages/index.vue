111d7912024d951a97bb93cb41bb48ed911c381d
harshita <harshita75rose@gmail.com>
2022-01-30 19:04:50 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <h2>
+      Welcome to the best corny dad jokes on the web
+    </h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  head() {
+        return {
+            title: 'Welcome To Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for dad jokes'
+                }
+            ]
+        };
+    } 
+};
+</script>
